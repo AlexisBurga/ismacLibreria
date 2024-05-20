57e46eadf62ee1cf23652ba5237638727d881c8b
MATUTINO <MATUTINO@DESKTOP-G7I3RU6>
2024-05-20 11:28:19 -0500

    ABV0001 -Primer cambio en git

---
+++ src/com/distribuida/entities/Categoria.java
@@ -0,0 +1,36 @@
+package com.distribuida.entities;
+
+public class Categoria {
+ //atributos
+	private int idCategoria;
+	private String categoria;
+	private String descripcion;
+	
+	public Categoria() {}
+	public Categoria(int idCategoria, String categoria, String descripcion) {
+	
+		this.idCategoria = idCategoria;
+		this.categoria = categoria;
+		this.descripcion = descripcion;
+	}
+	public int getIdCategoria() {
+		return idCategoria;
+	}
+	public void setIdCategoria(int idCategoria) {
+		this.idCategoria = idCategoria;
+	}
+	public String getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	
+	
+}
