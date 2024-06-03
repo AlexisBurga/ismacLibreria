75dcafa86d037cb0745ed77716c252dfefb45405
MATUTINO <MATUTINO@DESKTOP-G7I3RU6>
2024-06-03 11:48:59 -0500

    AB 0003 TAREA REALIZAR CATEGORIA Y AUTOR DAO Y LA IMPLEMENTACION

--- src/com/distribuida/dao/ClienteDaoImpl.java
+++ src/com/distribuida/dao/ClienteDaoImpl.java
@@ -2,17 +2,39 @@ package com.distribuida.dao;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.distribuida.entities.Cliente;
 
 @Repository
 public class ClienteDaoImpl implements ClienteDao {
+	
+	//Selec* from Clientes 
+	//select cl.* from clientes As cl -->> SQL
+	
+	
+	
+	
+	
+	@Autowired
+	private SessionFactory sessionFactory;
 
+	
+	
 	@Override
+	@Transactional
 	public List<Cliente> findAll() {
 		// TODO Auto-generated method stub
-		return null;
+		Session session= sessionFactory.getCurrentSession();
+		
+		
+		
+		return session.createQuery("from Cliente", Cliente.class).getResultList();
 	}
 
 	@Override
